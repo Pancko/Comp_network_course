736eb6421b62449dbb22875b6d7cfdd3bc60b545
Maou D Pancko <djaltair146@gmail.com>
2024-12-05 01:06:41 +0300

    1-4

---
+++ 3_TCP_CHAT/3_TCP_CHAT_SERVER.cpp
@@ -0,0 +1,151 @@
+//#undef UNICODE
+//
+//#define WIN32_LEAN_AND_MEAN
+//
+//#include <windows.h>
+//#include <winsock2.h>
+//#include <ws2tcpip.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <thread>
+//#include <vector>
+//#include <algorithm>
+//
+//// Need to link with Ws2_32.lib
+//#pragma comment (lib, "Ws2_32.lib")
+//// #pragma comment (lib, "Mswsock.lib")
+//
+//#define DEFAULT_BUFLEN 512
+//#define DEFAULT_PORT "80"
+//std::vector<SOCKET> clients; // Отличие от 2 задание - вектор клиентов для чата
+//
+//static int serverFunc(SOCKET ClientSocket) // Получает и печатает информацию
+//{
+//    int iResult;
+//    int iSendResult;
+//    char recvbuf[DEFAULT_BUFLEN];
+//    int recvbuflen = DEFAULT_BUFLEN;
+//    while (TRUE)
+//    {
+//        do
+//        {
+//            iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+//            if (iResult > 0)
+//            {
+//                printf("Bytes received: %d\n", iResult);
+//
+//                //Echo the buffer back to the sender
+//                for (auto client : clients)
+//                {
+//                    if (client != ClientSocket)
+//                        iSendResult = send(client, recvbuf, iResult, 0);
+//                    if (iSendResult == SOCKET_ERROR)
+//                    {
+//                        printf("send failed with error: %d\n", WSAGetLastError());
+//                        closesocket(ClientSocket);
+//                        WSACleanup();
+//                        return 1;
+//                    }
+//                    printf("Bytes sent: %d\n", iSendResult);
+//                }
+//            }
+//            else {
+//                if (WSAGetLastError() == WSAEWOULDBLOCK)
+//                {
+//                    Sleep(500);
+//                    continue;
+//                }
+//            }
+//
+//        } while (iResult > 0);
+//    }
+//
+//    closesocket(ClientSocket);
+//}
+//
+//
+//int __cdecl main(void)
+//{
+//    WSADATA wsaData;
+//    int iResult;
+//
+//    SOCKET ListenSocket = INVALID_SOCKET;// Сокет для прослушивания клиентов сервером
+//    SOCKET ClientSocket = INVALID_SOCKET; // Сокет клиента
+//
+//    struct addrinfo* result = NULL;
+//    struct addrinfo hints;
+//
+//    int iSendResult;
+//    char recvbuf[DEFAULT_BUFLEN];
+//    int recvbuflen = DEFAULT_BUFLEN;
+//
+//    // Initialize Winsock
+//    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+//    if (iResult != 0) {
+//        printf("WSAStartup failed with error: %d\n", iResult);
+//        return 1;
+//    }
+//
+//    ZeroMemory(&hints, sizeof(hints));
+//    hints.ai_family = AF_INET; //Ipv4
+//    hints.ai_socktype = SOCK_STREAM;
+//    hints.ai_protocol = IPPROTO_TCP;
+//    hints.ai_flags = AI_PASSIVE;
+//
+//    // Resolve the server address and port
+//    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+//    if (iResult != 0) {
+//        printf("getaddrinfo failed with error: %d\n", iResult);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Create a SOCKET for the server to listen for client connections.
+//    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+//    if (ListenSocket == INVALID_SOCKET) {
+//        printf("socket failed with error: %ld\n", WSAGetLastError());
+//        freeaddrinfo(result);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Setup the TCP listening socket
+//    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+//    if (iResult == SOCKET_ERROR) {
+//        printf("bind failed with error: %d\n", WSAGetLastError());
+//        freeaddrinfo(result);
+//        closesocket(ListenSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    freeaddrinfo(result);
+//
+//    iResult = listen(ListenSocket, SOMAXCONN); // Включаем прослушивание
+//    if (iResult == SOCKET_ERROR) {
+//        printf("listen failed with error: %d\n", WSAGetLastError());
+//        closesocket(ListenSocket);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    while (true) // Слушаем и принимаем клиента
+//    {
+//        // Accept a client socket
+//        ClientSocket = accept(ListenSocket, NULL, NULL); // Разрешает входящую попытку подключиться к сокету прослушивания
+//        if (ClientSocket == INVALID_SOCKET) {
+//            printf("accept failed with error: %d\n", WSAGetLastError());
+//            closesocket(ListenSocket);
+//            WSACleanup();
+//            return 1;
+//        }
+//
+//        clients.push_back(ClientSocket);
+//
+//        std::thread thread(serverFunc, ClientSocket);
+//        thread.detach();
+//    }
+//
+//    WSACleanup();
+//    return 0;
+//}
---
+++ 4_UDP_ECHO/4_UDP_ECHO_CLIENT.cpp
@@ -0,0 +1,94 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <thread>
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+#pragma warning(disable : 4996)
+
+#define DEFAULT_BUFLEN 512
+struct sockaddr_in SocketAddress; // Используется немного другая структура
+void sendMessageToServer(SOCKET Socket)
+{
+    char msg[DEFAULT_BUFLEN];
+    int iResult;
+
+    while (TRUE)
+    {
+        std::cin >> msg;
+
+        // Send an initial buffer
+        iResult = sendto(Socket, (const char*)msg, strlen(msg), 0, (SOCKADDR*)&SocketAddress, sizeof(SocketAddress));
+        if (iResult < 0) {
+            printf("send failed with error: %d\n", WSAGetLastError());
+            closesocket(Socket);
+            WSACleanup();
+            return;
+        }
+    }
+}
+
+void recieveMessageFromServer(SOCKET Socket)
+{
+    int iResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int size_SocketAddress = sizeof(SocketAddress);
+
+    while (TRUE)
+    {
+        // Receive until the peer closes the connection
+        do {
+            iResult = recvfrom(Socket, (char*)recvbuf, DEFAULT_BUFLEN, 0, (SOCKADDR*)&SocketAddress, &size_SocketAddress);
+            if (iResult > 0)
+            {
+                printf("Bytes received: %d\n", iResult);
+                recvbuf[iResult] = '\0'; // Добавить на конец принятого результата символ конца строки
+                std::cout << recvbuf << std::endl;
+            }
+            else if (iResult == 0)
+                printf("Connection closed\n");
+        } while (iResult > 0);
+    }
+}
+
+int __cdecl main(int argc, char** argv)
+{
+    WSADATA wsaData;
+    SOCKET Socket = INVALID_SOCKET;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    if ((Socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) // SOCK_DGRAM потому что UDP использует пакеты-датаграммы вместо потока как в TCP
+    {
+        printf("socket error%d\n", WSAGetLastError());
+        return 0;
+    }
+
+    ZeroMemory(&SocketAddress, sizeof(SocketAddress));
+    SocketAddress.sin_family = AF_INET; // IPv4
+    SocketAddress.sin_port = htons(12345); // Функция htons преобразует u_short из узла в порядок байтов сети TCP/IP (большой байт).
+    SocketAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    std::thread thread_send(sendMessageToServer, Socket);
+    std::thread thread_recieve(recieveMessageFromServer, Socket);
+    thread_send.join();
+    thread_recieve.join();
+
+    closesocket(Socket);
+    WSACleanup();
+    return 0;
+}
